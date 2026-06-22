965f7c03d89cc77b2fba9ca73a44b6de3d4dac86
Anton <KAC7200@yandex.ru>
2026-06-23 02:24:42 +0300

    #2 Fixed urls

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, SalesViewSet
+from .views import CategoryViewSet, ProductViewSet, SaleViewSet
 
 router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
-router.register(r'sales', SalesViewSet, basename='sale')
+router.register(r'sales', SaleViewSet, basename='sale')
 
 urlpatterns = [
     path('', include(router.urls)),
