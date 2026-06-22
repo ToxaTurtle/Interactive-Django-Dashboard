e9375b4f82ca739108b374244ee1c98e182e3bc8
Anton <KAC7200@yandex.ru>
2026-06-23 02:24:26 +0300

    #2 Updated Viewsets

--- core/views.py
+++ core/views.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
-from .models import Category, Product, Sales
-from .serializers import CategorySerializer, ProductSerializer, SalesSerializer
+from .models import Category, Product, Sale
+from .serializers import CategorySerializer, ProductSerializer, SaleSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -23,12 +23,11 @@ class ProductViewSet(viewsets.ModelViewSet):
     ordering_fields = ['price', 'name']
 
 
-class SalesViewSet(viewsets.ModelViewSet):
-    queryset = Sales.objects.select_related('category').all()
-    serializer_class = SalesSerializer
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.select_related('category').all()
+    serializer_class = SaleSerializer
     permission_classes = [IsAuthenticated]
 
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['status', 'payment_method', 'product', 'manager']
     ordering_field = ['created_at', 'total_price', 'quantity']
-    
