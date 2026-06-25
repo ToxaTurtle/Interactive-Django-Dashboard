be1139d678365be685c85fa527cad3475e928971
Anton <KAC7200@yandex.ru>
2026-06-25 18:28:22 +0300

    #3 Updated urls for front

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, SaleViewSet, RegisterView
+from .views import CategoryViewSet, ProductViewSet, SaleViewSet, RegisterView, UserViewSet
 
 router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'sales', SaleViewSet, basename='sale')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
