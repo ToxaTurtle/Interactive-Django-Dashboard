552a23e7ead9b1a00f8d8a73e578bdfa97e9d1b5
Anton <KAC7200@yandex.ru>
2026-06-29 20:08:30 +0300

    #2 Check for unauthenticated users

--- core/permissions.py
+++ core/permissions.py
@@ -5,11 +5,15 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in ('GET', 'HEAD', 'OPTIONS'):
             return True
+        if not request.user.is_authenticated:
+            return False
         return request.user.is_superuser or request.user.role == 'ADMIN'
 
 
 class IsManagerOrAdmin(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         if request.user.is_superuser or request.user.role == 'ADMIN':
             return True
         return request.user.role == 'MANAGER'
