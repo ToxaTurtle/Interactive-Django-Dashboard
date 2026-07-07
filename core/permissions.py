4c27241a3e9cd7bc0d2c73ba40e7bf3eab3f5af9
Anton <KAC7200@yandex.ru>
2026-07-07 16:52:40 +0300

    #3 Better logic and comments

--- core/permissions.py
+++ core/permissions.py
@@ -1,19 +1,34 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class IsAdminOrReadOnly(BasePermission):
+    """
+    Все пользователи могут просматривать (GET).
+    Но создавать/изменять (POST, PUT, DELETE) могут только Админы.
+    Отлично подходит для Каталога товаров и Категорий.
+    """
     def has_permission(self, request, view):
-        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+        if request.method in SAFE_METHODS:
             return True
-        if not request.user.is_authenticated:
-            return False
-        return request.user.is_superuser or request.user.role == 'ADMIN'
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_superuser or request.user.role == User.Role.ADMIN)
+        )
 
 
 class IsManagerOrAdmin(BasePermission):
+    """
+    Обычные юзеры не имеют доступа вообще.
+    Менеджеры и Админы могут просматривать и создавать (полный доступ).
+    Идеально подходит для эндпоинта Продаж.
+    """
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.is_superuser or request.user.role == 'ADMIN':
-            return True
-        return request.user.role == 'MANAGER'
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_superuser or request.user.role in [User.Role.ADMIN, User.Role.MANAGER])
+        )
